d188c776b26cb7b19a210d2e0af2ef75fda19ef6
Flavio Santos <flavio.santos.pro@gmail.com>
2019-12-14 15:16:31 -0300

    final types

--- AppCursoNelio/Program.cs
+++ AppCursoNelio/Program.cs
@@ -13,10 +13,27 @@ namespace AppCursoNelio
             int num2 = 2147483647;
             long num3 = 214748364754;
 
+            bool admin = false;
+            char gender = '\u0041';
+
+            float n4 = 4.5155f;
+            double n5 = 4.5;
+
+            string eliza = "queen";
+            eliza += "jose";
+
+
+
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(eliza);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
             Console.WriteLine(x);
             Console.WriteLine(num);
             Console.WriteLine(num2);
             Console.WriteLine(num3);
+            Console.WriteLine(gender);
         }
     }
 }
