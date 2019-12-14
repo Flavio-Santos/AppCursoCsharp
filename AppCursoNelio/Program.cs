14dc09caef8fcd77c843b79eeb55b8af4203f5f8
Flavio Santos <flavio.santos.pro@gmail.com>
2019-12-14 14:32:47 -0300

    init study

---
+++ AppCursoNelio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppCursoNelio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
